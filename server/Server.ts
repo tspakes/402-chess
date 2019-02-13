8544412d07e67c24dd544da75839b4588ef4e0e2
Ben Sergent V <bsergent21897@gmail.com>
2019-02-13 14:01:53 -0500

    Fix routing for static file serving and API endpoints

--- server/Server.ts
+++ server/Server.ts
@@ -13,12 +13,13 @@ class Server {
 	constructor() {
 		this.app = Express();
 		this.router = Express.Router();
+		this.app.use(this.router);
 		this.config();
 		this.route();
 
 		this.app.listen(PORT, () => {
 			console.log('Express server listening on port ' + PORT);
-			console.log('path=' + __dirname);
+			console.log('runtime_path=' + __dirname);
 		});
 	}
 
@@ -30,63 +31,32 @@ class Server {
 	}
 
 	private route(): void {
-		// this.router.use((req: Request, res: Response, next: Function) => {
-		// 	// Run this on every incoming request
-		// 	next();
-		// });
-
-		// TODO Route through other routers first that check if the request matches the APIs
-
-		// Website file serving
-		// this.app.use('/web', express.static(__dirname + '/www'));
-		console.log(Path.join(__dirname, '../www'));
-		this.app.use(
-			(req: Request, res: Response, next: Function) => {
-				if (!req.baseUrl.startsWith('/board/')) {
-					next();
-					return;
-				}
-				// Board API stuff
-				res.status(200);
-				res.send('Board API');
-			},
-			(req: Request, res: Response, next: Function) => {
-				if (!req.baseUrl.startsWith('/stats/')) {
-					next();
-					return;
-				}
-				// Statistics API stuff
-				res.status(200);
-				res.send('Statistics API');
-			},
-			Express.static(Path.join(__dirname, '../www'))
-		); // Use Path.join here to handle the \ and / b/w Windows and all other OS
-		// this.app.use('/raw', express.static(__dirname + '/'));
-		// this.app.use('/web', express.static(__dirname + '/www'));
-		// this.app.get('/', (req: Request, res: Response) => {
-		// 	res.send('Hello world');
-		// });
+		this.router.use((req: Request, res: Response, next: Function) => {
+			// Run this on every incoming request
+			console.log('req_url=' + req.url);
+			next();
+		});
 
 		// Board API
-		// this.app.use('/board', this.router);
-		// this.router.get('/board', (req: Request, res: Response) => {
-			
-		// 	res.status(200);
-		// 	res.json({
-		// 		route: 'BoardAPI',
-		// 		message: 'Success!'
-		// 	});
-		// });
+		this.app.get('/board', (req: Request, res: Response) => {
+			res.status(200);
+			res.json({
+				route: 'BoardAPI',
+				message: 'Success!'
+			});
+		});
 
-		// // Database API
-		// this.app.use('/stats', this.router);
-		// this.router.get('/stats', (req: Request, res: Response) => {
-		// 	res.status(200);
-		// 	res.json({
-		// 		route: 'Database',
-		// 		message: 'Success!'
-		// 	});
-		// });
+		// Database API
+		this.app.get('/stats', (req: Request, res: Response) => {
+			res.status(200);
+			res.json({
+				route: 'Database',
+				message: 'Success!'
+			});
+		});
+
+		// Website file serving
+		this.app.use(Express.static(Path.join(__dirname, '../www')));
 	}
 }
 
