639ee25747df9153a3ed7bf32095738a2c0251b3
Eli Alan Harris <eli.harris@immersacad.com>
2019-03-28 08:57:16 -0400

    Pawn Promotion Bug Fix

--- angular-chess/src/app/app.component.ts
+++ angular-chess/src/app/app.component.ts
@@ -38,6 +38,8 @@ export class AppComponent implements OnInit, OnDestroy {
 
             this.board = board.board;
 
+            console.log(this.board);
+
             this.checkMessages();
         });
 
@@ -55,8 +57,10 @@ export class AppComponent implements OnInit, OnDestroy {
     private checkMessages() {
 
         if (this.board !== null) {
-            if (this.board.message === 'PAWN_PROMOTION' && this.modalRef === null) {
-                this.modalRef = this.modalService.show(PromotionModalComponent);
+            if (this.board.message === 'PAWN_PROMOTION') {
+                if (this.modalRef === null) {
+                    this.modalRef = this.modalService.show(PromotionModalComponent);
+                }
             } else if (this.modalRef !== null) {
                 this.modalRef.hide();
                 this.modalRef = null;
